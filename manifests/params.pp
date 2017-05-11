289da6914c91f3441a7baeb87c3b654ea622554a
Daniel Farrell <dfarrell@redhat.com>
2017-05-11 10:28:43 -0400

    Revert "Adds ability to configure ODL user/pass"
    Not supported in Boron, removing from stable/boron banch.
    
    The JAR this used to change ODL's user/pass isn't in Boron and will not
    be added in an SR.
    
    This reverts commit 9e22e9e083861c614353fc0b96ca9f82c504e5e6.
    
    Change-Id: I84cd5c3c37a85f4dcd1691526cf426d197ea6c62
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>
    (cherry picked from commit d5aa2a373fabbed351a559feaeb6b45ce1347e09)

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,6 +22,4 @@ class opendaylight::params {
   $vpp_routing_node = ''
   $java_opts = '-Djava.net.preferIPv4Stack=true'
   $manage_repositories = true
-  $username = 'admin'
-  $password = 'admin'
 }
